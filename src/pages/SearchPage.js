00ce2ed00a03f4b7f60546f09c206030bcf88c96
Justin Spegele <justin.spegele@gmail.com>
2021-09-20 12:39:10 -0400

    Added Container to search page

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,28 +1,14 @@
 import React from 'react'
+
+import Container from '../components/Container'
 import ClanSearch from '../components/ClanSearch'
 
-const SearchPage = () => {
-  // test fetch
-  // fetch("https://www.bungie.net/Platform/User/Search/Prefix/TheMachoMan/0/", {
-  //     headers: {
-  //       'X-API-Key': process.env.REACT_APP_BUNGIE_API_KEY
-  //     }
-  //   })
-  //   .then(res => res.json())
-  //   .then(
-  //     (result) => {
-  //       console.log('test fetch', result)
-  //     },
-  //     (error) => {
-  //       console.log('test fetch error', error)
-  //     }
-  //   )
-  
+const SearchPage = () => {  
   return (
-    <div>
+    <Container>
       <h2>Clan Search</h2>
       <ClanSearch />
-    </div>
+    </Container>
   )
 }
  
